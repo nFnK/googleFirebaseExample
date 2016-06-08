9f3e8766de6d9353e8fb2279e572de2d2ed86ee9
jeagle <joshuaseagle@gmail.com>
2016-06-08 19:10:41 -0400

    I created an alarm example for a friend

--- js/firebaseTest.js
+++ js/firebaseTest.js
@@ -9,8 +9,11 @@ var db,
         databaseURL: "https://example-a01c4.firebaseio.com",
         storageBucket: "example-a01c4.appspot.com",
     },
-    elCountInput = document.querySelector("#user-count");
-    elCountBtn = document.querySelector("#user-count-btn");
+    //elCountInput = document.querySelector("#user-count");
+    elAlarmClock = document.querySelector(".alarm-wrapper img"),
+    elOnAlarmBtn = document.querySelector(".on-alarm"),
+    elOffAlarmBtn = document.querySelector(".off-alarm"),
+    elAlarmSound = document.querySelector("#alarm-sound");
 
 // Initialize your instance of firebase    
 firebase.initializeApp(config);
@@ -24,13 +27,23 @@ db = firebase.database();
 */
 db.ref('userPopulation/userCount').on('value', function(snapshot) {
   console.log(snapshot);
-  document.querySelector(".count").innerHTML = snapshot.val();
+  if (snapshot.val() === 1) {
+    elAlarmClock.className = "shake";
+  } else {
+    elAlarmClock.classList.remove("shake"); 
+  }
+  //document.querySelector(".count").innerHTML = snapshot.val();
 });
 
 /*
    You will most likely not need this, but this is an event listener for the click of the button I made
    It will save a new value into firebase. I did this to demonstrate how awesome it works. :)
 */
-elCountBtn.addEventListener("click", function () {
-   db.ref('userPopulation/userCount').set(elCountInput.value); 
+elOnAlarmBtn.addEventListener("click", function () {
+   db.ref('userPopulation/userCount').set(1); 
+   elAlarmSound.innerHTML = "<embed src='sounds/play.mp3' autostart=true loop=true volume=100 hidden=true>";
+});
+elOffAlarmBtn.addEventListener("click", function () {
+   db.ref('userPopulation/userCount').set(0); 
+   elAlarmSound.innerHTML = "";
 });
