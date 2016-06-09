b5d862a327a18725ff6e39dc9bed80dcda13b7bc
jeagle <joshuaseagle@gmail.com>
2016-06-08 22:58:29 -0400

    Added the code the retrieves the data from Firebase, and appends it to the dom

--- js/firebaseTest.js
+++ js/firebaseTest.js
@@ -10,8 +10,8 @@
             databaseURL: "https://example-a01c4.firebaseio.com",
             storageBucket: "example-a01c4.appspot.com",
         },
-        elCountInput = document.querySelector("#user-count");
-        elCountBtn = document.querySelector("#user-count-btn");
+        elMessages = document.querySelector(".messages"),
+        elSendBtn = document.querySelector(".send");
 
     // Initialize your instance of firebase    
     firebase.initializeApp(config);
@@ -23,16 +23,21 @@
         This is a firebase reference that grabs my json object and also by default refreshes the value on the page
         which I pass into the callback function. This is where all of the magic happens.
     */
-    db.ref('userPopulation/userCount').on('value', function(snapshot) {
-    console.log(snapshot);
-    document.querySelector(".count").innerHTML = snapshot.val();
+    db.ref('messages/').on('value', function(snapshot) {
+        var el = document.createElement('dl');
+        
+        for (var i = 0; i < snapshot.val().length; i++) {
+            el.innerHTML += ("<dt>" + snapshot.val()[i].name + "</dt><dd>" + snapshot.val()[i].text + "</dd>");
+        }
+            elMessages.innerHTML = '';
+            elMessages.appendChild(el);
     });
 
     /*
-    You will most likely not need this, but this is an event listener for the click of the button I made
-    It will save a new value into firebase. I did this to demonstrate how awesome it works. :)
+        You will most likely not need this, but this is an event listener for the click of the button I made
+        It will save a new value into firebase. I did this to demonstrate how awesome it works. :)
     */
-    elCountBtn.addEventListener("click", function () {
-    db.ref('userPopulation/userCount').set(elCountInput.value); 
+    elSendBtn.addEventListener("click", function () {
+        db.ref('messages/' + 0 + '/name').set("James"); 
     });
 })();
